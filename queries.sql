77cd6419b939128c9ca6587102633de846206ab2
Matthew Eckes <eckesm@gmail.com>
2021-02-22 14:01:01 -0500

    completed assignment and solution review

---
+++ queries.sql
@@ -0,0 +1,18 @@
+-- write your queries here
+
+-- 1
+SELECT * FROM owners o
+FULL JOIN vehicles v ON o.id = v.owner_id
+
+-- 2
+SELECT o.first_name, o.last_name, COUNT(*) AS num_vehicles FROM owners o
+JOIN vehicles v ON o.id = v.owner_id
+GROUP BY (o.first_name,o.last_name)
+ORDER BY o.first_name
+
+-- 3
+SELECT o.first_name,o.last_name,AVG(v.price) AS average_price,COUNT(*) FROM owners o
+JOIN vehicles v ON o.id = v.owner_id
+GROUP BY (o.first_name, o.last_name)
+HAVING COUNT(*) > 1 AND ROUND(AVG(v.price)) > 10000
+ORDER BY o.first_name DESC
